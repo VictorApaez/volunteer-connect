7aecc630517f93a44d924cc5be3fbe8d559c9bcc
VictorApaez <vic.apaez@gmail.com>
2023-03-15 20:19:45 -0400

    created a service for getting comments based on the post

--- client/src/services/commentsService.js
+++ client/src/services/commentsService.js
@@ -3,6 +3,25 @@ import api from "./apiConfig";
 export const createComment = async ({ postId, content }) => {
   try {
     const response = await api.post("/comments/create", { postId, content });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// export const getCommentsByPostId = async (postId) => {
+//   try {
+//     const res = api.get("id/all", { postId });
+//     return res;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+export const getAllComments = async (postId) => {
+  try {
+    const response = await api.get("/comments/all", { postId });
     return response.data;
   } catch (error) {
     console.log(error);
