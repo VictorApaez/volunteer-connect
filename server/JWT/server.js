d7879753ba404ba78f7a98e62d48e47ac9de1607
VictorApaez <vic.apaez@gmail.com>
2023-04-17 12:08:31 -0400

    moved line

--- server/JWT/server.js
+++ server/JWT/server.js
@@ -7,12 +7,6 @@ const postRoutes = require("./routes/posts");
 const commentsRoutes = require("./routes/comments");
 
 app.use(cors());
-app.use(express.json());
-
-app.use("/auth", authRoutes);
-app.use("/posts", postRoutes);
-app.use("/comments", commentsRoutes);
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -25,6 +19,11 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/posts", postRoutes);
+app.use("/comments", commentsRoutes);
 
 const port = 5000;
 app.listen(port, () => {
