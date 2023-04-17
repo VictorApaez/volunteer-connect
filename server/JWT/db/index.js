76d41ae68342b978cc3f924513a3bf39a42b478f
VictorApaez <vic.apaez@gmail.com>
2023-04-17 12:02:51 -0400

    added headers to allow HTTP req

--- server/JWT/db/index.js
+++ server/JWT/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI;
-const testingURL = "mongodb://localhost/jwtapp";
+// const testingURL = "mongodb://localhost/jwtapp";
 
 mongoose.set("strictQuery", false);
 mongoose
--- server/JWT/server.js
+++ server/JWT/server.js
@@ -13,6 +13,19 @@ app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
 app.use("/comments", commentsRoutes);
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
