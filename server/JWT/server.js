a0d82059b29b2bec9ee4ad0578b9050315e2de86
VictorApaez <vic.apaez@gmail.com>
2023-04-17 12:11:32 -0400

    config cors

--- server/JWT/server.js
+++ server/JWT/server.js
@@ -5,20 +5,14 @@ const authRoutes = require("./routes/auth");
 const cors = require("cors");
 const postRoutes = require("./routes/posts");
 const commentsRoutes = require("./routes/comments");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
