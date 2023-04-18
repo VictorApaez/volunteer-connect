7d999e87d775c22182bd0b71e6a2de6f4b13dd0d
VictorApaez <vic.apaez@gmail.com>
2023-04-18 08:37:06 -0400

    refactored signup and login

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -2,32 +2,20 @@ import api from "./apiConfig";
 
 export const signup = async ({ username, password }) => {
   try {
-    const response = await api.post("/auth/signup", {
-      username,
-      password,
-    });
+    const response = await api.post("/auth/signup", { username, password });
     if (response.status === 200) {
-      // Signup was successful, log in the user
-      const loginResponse = await login({ username, password });
-
-      if (loginResponse.success) {
-        return {
-          success: true,
-          token: loginResponse.token,
-          error: null,
-          status: response.status,
-        };
-      } else {
-        return loginResponse;
-      }
+      return {
+        success: true,
+        token: response.data.token,
+        error: null,
+        status: response.status,
+      };
     }
   } catch (err) {
     const { data, status } = err.response;
     if (status === 409) {
-      // Username already exists, return error message
       return { success: false, error: data.message, status, token: null };
     } else {
-      // Some other error occurred
       return {
         success: false,
         error: "An error occurred during signup",
@@ -40,15 +28,8 @@ export const signup = async ({ username, password }) => {
 
 export const login = async ({ username, password }) => {
   try {
-    const response = await api.post("/auth/login", {
-      username,
-      password,
-    });
-    if (response.status === 200) {
-      return { success: true, token: response.data.token };
-    } else {
-      return { success: false, error: response.data.message };
-    }
+    const response = await api.post("/auth/login", { username, password });
+    return { success: true, token: response.data.token };
   } catch (err) {
     return { success: false, error: err.response.data };
   }
